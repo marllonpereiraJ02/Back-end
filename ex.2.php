5ed1653f07a51362047a5e51b420cf4a481b4bce
MARLLON PEREIRA <marllon.pereira@aluno.senai.br>
2026-08-27 17:20:50 -0300

    Add files via upload

---
+++ ex.2.php
@@ -0,0 +1,43 @@
+<?php
+
+$nome1 = "João Lucas";
+$nome2 = "Ana Barbosa";
+$nome3 = "Mario Miguel";
+$nome4 = "Julia Soares Roseto";
+
+$idade1 = 36;
+$idade2 = 26;
+$idade3 = 32;
+$idade4 = 45;
+
+$sexo1 = "M";
+$sexo2 = "F";
+$sexo3 = "M";
+$sexo4 = "F";
+
+$ensinosuperior1 = "N";
+$ensinosuperior2 = "S";
+$ensinosuperior3 = "S";
+$ensinosuperior4 = "S";
+
+echo "Meu nome é ".$nome1."<br>";
+echo "Tenho $idade1 anos de idade<br>";
+echo "Do gênero $sexo1<br>";
+echo "Ensino superior $ensinosuperior1 completo<br><br>";
+
+echo "Meu nome é ".$nome2."<br>";
+echo "Tenho $idade2 anos de idade<br>";
+echo "Do gênero $sexo2<br>";
+echo "Ensino superior $ensinosuperior2 completo<br><br>";
+
+echo "Meu nome é ".$nome3."<br>";
+echo "Tenho $idade3 anos de idade<br>";
+echo "Do gênero $sexo3<br>";
+echo "Ensino superior $ensinosuperior3 completo<br><br>";
+
+echo "Meu nome é ".$nome4."<br>";
+echo "Tenho $idade4 anos de idade<br>";
+echo "Do gênero $sexo4<br>";
+echo "Ensino superior $ensinosuperior4 completo<br><br>";
+
+?>
---
+++ ex1.php
@@ -0,0 +1,15 @@
+<?php
+
+$nome = "Marllon Pereira";
+$idade = 16;
+$salario = 650;
+$sexo = "M";
+$profissao = "Jovem Aprendiz";
+
+echo "Meu nome é ".$nome;
+echo"<br>";
+echo"Tenho $idade anos de idade";
+echo"<br>";
+echo"Ganho $salario e eu sou $profissao";
+echo "<br>";
+?>
